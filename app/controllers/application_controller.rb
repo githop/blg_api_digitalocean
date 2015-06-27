3584d90b1cc1c57d6c7a7098ac8c56bc3879c831
Tom Hopkins <tom@githop.com>
2015-06-27 17:36:17 -0600

    setup authenticate method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+	protect_from_forgery with: :null_session
+
+	attr_reader :current_user
+
+	def authenticate
+		cmd = AuthenticateUser.call(get_auth_token)
+		if cmd.success?
+			@current_user = cmd.result
+		else
+			render json: {error: cmd.error}, status: :unauthorized
+			false
+		end
+	end
+
+	private
+
+	def get_auth_token
+		auth_header = request.headers['Authorization'].split(' ').last
+	end
+
 end

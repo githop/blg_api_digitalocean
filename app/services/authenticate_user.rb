bfc78a1d9cda30b3ce671369031c7a8a2980f440
Tom Hopkins <tom@githop.com>
2015-06-28 01:10:17 -0600

    add - modify authenticate_user to handle missing authorization token

--- app/services/authenticate_user.rb
+++ app/services/authenticate_user.rb
@@ -3,20 +3,29 @@ require 'token_handler'
 class AuthenticateUser
 	prepend SimpleCommand
 
-	attr_reader :token
+	attr_reader :headers
 
-	def initialize(token)
-		@token = token
+	def initialize(headers = {})
+		@headers = headers
 	end
 
 	def call
-		user, _ = TokenHander.decode_auth_token(token)
+		token = http_auth_header
+		user, _ = TokenHandler.decode_auth_token(token)
 		user
 	rescue JWT::ExpiredSignature
 		errors.add(:base, "The token is expired")
 		nil
 	rescue JWT::DecodeError
 		errors.add(:base, "the token is invalid")
+	end
+
+	def http_auth_header
+		if headers['Authorization'].present?
+			auth_header = headers['Authorization'].split(' ').last
+		else
+			errors.add(:base, 'Missing token')
+		end
 	end
 
 	def error

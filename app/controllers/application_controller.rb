8d51e64e97eaae559226e01c156ef22638755785
Tom Hopkins <tom@githop.com>
2015-06-28 01:11:12 -0600

    add - POC protected endpoint, it works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,13 @@ class ApplicationController < ActionController::API
 	attr_reader :current_user
 
 	def authenticate
-		cmd = AuthenticateUser.call(get_auth_token)
+		cmd = AuthenticateUser.call(request.headers)
 		if cmd.success?
 			@current_user = cmd.result
 		else
 			render json: {error: cmd.error}, status: :unauthorized
 			false
 		end
-	end
-
-	private
-
-	def get_auth_token
-		auth_header = request.headers['Authorization'].split(' ').last
 	end
 
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate, only: :create
+
 	def index
 		@articles = Article.includes(:imgs, headers: :paragraphs)
 		render json: @articles, each_serializer: ArticleSerializer, include: 'imgs,headers,paragraphs'
@@ -7,5 +9,9 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.includes(:imgs, headers: :paragraphs).find(params[:id])
 		render json: @article, serializer: ArticleSerializer, include: 'imgs,headers,paragraphs'
+	end
+
+	def create
+		render json: {indeed: "it worked"}
 	end
 end

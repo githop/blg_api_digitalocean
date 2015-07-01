57149578839775d5da685626d4e384a3a1407f72
Tom Hopkins <tom@githop.com>
2015-07-01 02:10:57 -0600

    allow camelCase attribute name

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,6 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :posted_on
+  attributes :id, :title
+  attribute :posted_on, :key => :postedOn
 
   has_many :imgs, :headers, :paragraphs
   belongs_to :user
